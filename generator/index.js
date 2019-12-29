818645cb398452fc8fe2ee1befdc4930b5400775
hp8hepeng <hp8hepeng@163.com>
2019-12-29 21:48:40 +0800

    提交

--- generator/index.js
+++ generator/index.js
@@ -62,11 +62,11 @@ module.exports = (api, options, rootOptions) => {
 
   // 复制并用 ejs 渲染 `./template` 内所有的文件
   if (options.type === '3') {
+    console.log(`感谢使用3.0模板,有问题请联系何鹏`)
     api.render('../template/3.0')
     // options.module 可以访问上面问题数组的第一个对象的值，默认为: 'module0'
-    console.log(`感谢使用3.0模板,有问题请联系何鹏`)
   } else if (options.type === '2') {
-    api.render('../template/2.0')
-    console.log('感谢使用2.x模板,有问题请联系何鹏')
+    api.render('../template/3.0')
+    console.log('2.0无人维护 默认安装3.0项目')
   }
 }
